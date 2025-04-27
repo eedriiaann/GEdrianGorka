739a56cbf83a93714bea3c801b0cd9040f929f7f
gorka <gorka@DESKTOP-7EKUIUG>
2025-04-27 16:58:10 +0200

    Septimo cambio

---
+++ src/Metodos/metodo.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Metodos;
+
+/**
+ *
+ * @author gorka
+ */
+public class metodo {
+    public int resta (int a, int b){
+        int c;
+        c = a-b;
+        return c;
+    }
+}
